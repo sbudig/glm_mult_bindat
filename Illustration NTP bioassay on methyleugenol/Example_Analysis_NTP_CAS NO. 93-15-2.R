7b7438179d10cb61fd341219ebe193c2752068db
sbudig <110831972+sbudig@users.noreply.github.com>
2023-05-15 16:43:12 +0200

    Update

--- Illustration NTP bioassay on methyleugenol/Example_Analysis_NTP_CAS NO. 93-15-2.R
+++ Illustration NTP bioassay on methyleugenol/Example_Analysis_NTP_CAS NO. 93-15-2.R
@@ -15,6 +15,11 @@
 # f344/n rats and b6c3f1 mice (gavage studies). National Toxicology Program technical
 # report series, 491:1-412
 
+# Short Overview:
+# First the data is imported and prepared for further analysis.
+# Then appropariate GLMs or BayesGLMs are fitted for each tumour and combined into a list.
+# Then the p-value adjustments are performed with the respective method
+
 # Setup -------------------------------------------------------------------
 
 #setwd("")
@@ -62,16 +67,16 @@ fitlist_glm <- list(t24_glm, t26_glm, t27_glm, t28_glm, t29_glm,
 library(arm)
 
 ## Bayesian generalized linear models are fitted to each of the tumor class 
-t24_bglm <- glm(cbind(t24,1-t24)~dose, data = tt5, family = binomial())
-t26_bglm <- glm(cbind(t26,1-t26)~dose, data = tt5, family = binomial())
-t27_bglm <- glm(cbind(t27,1-t27)~dose, data = tt5, family = binomial())
-t28_bglm <- glm(cbind(t28,1-t28)~dose, data = tt5, family = binomial())
-t29_bglm <- glm(cbind(t29,1-t29)~dose, data = tt5, family = binomial())
-t36_bglm <- glm(cbind(t36,1-t36)~dose, data = tt5, family = binomial())
-t41_bglm <- glm(cbind(t41,1-t41)~dose, data = tt5, family = binomial())
-t42_bglm <- glm(cbind(t42,1-t42)~dose, data = tt5, family = binomial())
-t59_bglm <- glm(cbind(t59,1-t59)~dose, data = tt5, family = binomial())
-t71_bglm <- glm(cbind(t71,1-t71)~dose, data = tt5, family = binomial())
+t24_bglm <- bayesglm(cbind(t24,1-t24)~dose, data = tt5, family = binomial())
+t26_bglm <- bayesglm(cbind(t26,1-t26)~dose, data = tt5, family = binomial())
+t27_bglm <- bayesglm(cbind(t27,1-t27)~dose, data = tt5, family = binomial())
+t28_bglm <- bayesglm(cbind(t28,1-t28)~dose, data = tt5, family = binomial())
+t29_bglm <- bayesglm(cbind(t29,1-t29)~dose, data = tt5, family = binomial())
+t36_bglm <- bayesglm(cbind(t36,1-t36)~dose, data = tt5, family = binomial())
+t41_bglm <- bayesglm(cbind(t41,1-t41)~dose, data = tt5, family = binomial())
+t42_bglm <- bayesglm(cbind(t42,1-t42)~dose, data = tt5, family = binomial())
+t59_bglm <- bayesglm(cbind(t59,1-t59)~dose, data = tt5, family = binomial())
+t71_bglm <- bayesglm(cbind(t71,1-t71)~dose, data = tt5, family = binomial())
 
 ## Bglms are combined into list for further analysis
 fitlist_bglm <- list(t24_bglm, t26_bglm, t27_bglm, t28_bglm, t29_bglm, 
@@ -80,29 +85,25 @@ fitlist_bglm <- list(t24_bglm, t26_bglm, t27_bglm, t28_bglm, t29_bglm,
 
 # Bonferroni adjustment (GLM) ---------------------------------------------
 
-## p-values are extracted from summary 
-coeflist_glm <- lapply(lapply(fitlist_glm, summary), coef)
-coeflist_glm <- do.call(rbind.data.frame,(coeflist_glm))[,4]
-
-## seq is used to just extract slope p-values
-seq <- seq(from = 2, to = length(coeflist_glm), by = 2)
+## p-values for each model are extracted from summary 
+p_vec_glm <- lapply(fitlist_glm, function(x) summary(x)$coefficients[8])
 
 ## adjusted p-values are obtained with p.adjust function 'method = "bonferroni"'
-p_adj_glm_bonf <- p.adjust(coeflist_glm[seq], method = "bonferroni")
+p_adj_glm_bonf <- p.adjust(p_vec_glm, method = "bonferroni")
+
+## The sorting of the p-values corresponds to the order of the models in the list.
 p_adj_glm_bonf
 
 
 # Bonferroni adjustment (BGLM) --------------------------------------------
 
-## p-values are extracted from summary
-coeflist_bglm <- lapply(lapply(fitlist_bglm, summary), coef)
-coeflist_bglm <- do.call(rbind.data.frame,(coeflist_bglm))[,4]
-
-## seq is used to just extract slope p-values
-seq <- seq(from = 2, to = length(coeflist_bglm), by = 2)
+## p-values for each model are extracted from summary 
+p_vec_bglm <- lapply(fitlist_bglm, function(x) summary(x)$coefficients[8])
 
 ## adjusted p-values are obtained with p.adjust function 'method = "bonferroni"'
-p_adj_bglm_bonf <- p.adjust(coeflist_bglm[seq], method = "bonferroni")
+p_adj_bglm_bonf <- p.adjust(p_vec_bglm, method = "bonferroni")
+
+## The sorting of the p-values corresponds to the order of the models in the list.
 p_adj_bglm_bonf
 
 
@@ -117,6 +118,7 @@ mmm_glm <- mmm(t24_glm, t26_glm, t27_glm, t28_glm, t29_glm,
                 t36_glm, t41_glm, t42_glm, t59_glm, t71_glm)
 
 ## specify the matrix with the hypotheses to be tested
+## we just want to test every second coefficient (only the slope, not the intercept)
 seq_mmm <- seq(from=2, to= 2*length(mmm_glm), by = 2)
 K <- diag(length(coef(mmm_glm)))[seq_mmm,]
 rownames(K) <- names(coef(mmm_glm))[seq_mmm]
@@ -124,6 +126,7 @@ rownames(K) <- names(coef(mmm_glm))[seq_mmm]
 ## use function 'glht' with the matrix and 'summary' to obtain the adjusted p-values
 mmm_glht_glm <- glht(mmm_glm, linfct = K)
 p_adj_glm_mmm <- summary(mmm_glht_glm)
+
 p_adj_glm_mmm
 
 
@@ -138,6 +141,7 @@ mmm_bglm <- mmm(t24_bglm, t26_bglm, t27_bglm, t28_bglm, t29_bglm,
                t36_bglm, t41_bglm, t42_bglm, t59_bglm, t71_bglm)
 
 ## specify the matrix with the hypotheses to be tested
+## we just want to test every second coefficient (only the slope, not the intercept)
 seq_mmm <- seq(from=2, to= 2*length(mmm_bglm), by = 2)
 K <- diag(length(coef(mmm_bglm)))[seq_mmm,]
 rownames(K) <- names(coef(mmm_bglm))[seq_mmm]
@@ -145,27 +149,30 @@ rownames(K) <- names(coef(mmm_bglm))[seq_mmm]
 ## use function 'glht' with the matrix and 'summary' to obtain the adjusted p-values
 mmm_glht_bglm <- glht(mmm_bglm, linfct = K)
 p_adj_bglm_mmm <- summary(mmm_glht_bglm)
+
 p_adj_bglm_mmm
 
 
 # Bootstrap adjustment (GLM) ----------------------------------------------
 
 ## extract the p-values of interest with the summary function and store into vector
-sumlist_bt <- lapply(fitlist_glm, function(x) summary(x)$coefficients[8])
-p_vec <- as.vector(unlist(sumlist_bt))
+l_pvalues_glm <- lapply(fitlist_glm, function(x) summary(x)$coefficients[8])
+v_pvalues_glm <- as.vector(unlist(l_pvalues_glm))
 
 ## J is the number of endpoints
-J <- length(p_vec)
+J <- length(v_pvalues_glm)
 
-## specify the matrix where bootstrap results are stored and set number of bootstraps
-BPM <- matrix(nrow = 0, ncol = length(p_vec))
+## specify the matrix where bootstrap results are stored
+BPM <- matrix(nrow = 0, ncol = length(v_pvalues_glm))
+
+# set number of bootstrap iterations
 nboot <- 10000
 
 for (b in 1:nboot) {
   
   ## resample data for each of the tumor vectors
   RSDAT <- apply(tt5[seq(from = 1, to = J, by = 1)], 2, sample)
-  RFDAT <- size-RSDAT
+  RFDAT <- 1-RSDAT
   
   ## create new data.frame with the resampled data and success/occurence and failure/non-occurence
   colnames(RSDAT) <- paste("s",1:J, sep="")
@@ -191,35 +198,39 @@ for (b in 1:nboot) {
   ## extract the minimum p-value and check whether it is smaller 
   ## than the respective original unadjusted p-values.  
   ## Note the result for the respective endpoint
-  BPM <- rbind(BPM,as.integer(min(RSPVEC) <= p_vec))
+  BPM <- rbind(BPM,as.integer(min(RSPVEC) <= v_pvalues_glm))
   
 }
 
 ## calculate how many times the minimum p value from the respective bootstrap run was smaller 
 ## than the respective unadjusted p-value
 
-p_adj_glm_bt <- colSums(BPM)/10000
+p_adj_glm_bt <- colSums(BPM)/nboot
+
+## The sorting of the p-values corresponds to the order of the models in the list.
 p_adj_glm_bt
 
 
 # Bootstrap adjustment (BGLM) ---------------------------------------------
 
 ## extract the p-values of interest with the summary function and store into vector
-sumlist_bt <- lapply(fitlist_bglm, function(x) summary(x)$coefficients[8])
-p_vec <- as.vector(unlist(sumlist_bt))
+l_pvalues_bglm <- lapply(fitlist_bglm, function(x) summary(x)$coefficients[8])
+v_pvalues_bglm <- as.vector(unlist(l_pvalues_bglm))
 
 ## J is the number of endpoints
-J <- length(p_vec)
+J <- length(v_pvalues_bglm)
 
-## specify the matrix where bootstrap results are stored and set number of bootstraps
-BPM <- matrix(nrow = 0, ncol = length(p_vec))
+## specify the matrix where bootstrap results are stored
+BPM <- matrix(nrow = 0, ncol = length(v_pvalues_bglm))
+
+# set number of bootstrap iterations
 nboot <- 10000
 
 for (b in 1:nboot) {
   
   ## resample data for each of the tumor vectors
   RSDAT <- apply(tt5[seq(from = 1, to = J, by = 1)], 2, sample)
-  RFDAT <- size-RSDAT
+  RFDAT <- 1-RSDAT
   
   ## create new data.frame with the resampled data and success/occurence and failure/non-occurence
   colnames(RSDAT) <- paste("s",1:J, sep="")
@@ -236,7 +247,7 @@ for (b in 1:nboot) {
   RSFITLIST <- FORMLIST
   
   ## fit glms for the respective endpoints
-  for(k in 1:J){RSFITLIST[[k]] <- glm(FORMLIST[[k]], data=RSDF, family=binomial)}
+  for(k in 1:J){RSFITLIST[[k]] <- bayesglm(FORMLIST[[k]], data=RSDF, family=binomial)}
   
   ## extract p-values of interest and store into vector
   RSSUMLIST <- lapply(RSFITLIST, function(x) summary(x)$coefficients[8])
@@ -245,17 +256,19 @@ for (b in 1:nboot) {
   ## extract the minimum p-value and check whether it is smaller 
   ## than the respective original unadjusted p-values.  
   ## Note the result for the respective endpoint
-  BPM <- rbind(BPM,as.integer(min(RSPVEC) <= p_vec))
+  BPM <- rbind(BPM,as.integer(min(RSPVEC) <= v_pvalues_bglm))
   
 }
 
 ## calculate how many times the minimum p value from the respective bootstrap run was smaller 
 ## than the respective unadjusted p-value
 
-p_adj_bglm_bt <- colSums(BPM)/10000
+p_adj_bglm_bt <- colSums(BPM)/nboot
+
+## The sorting of the p-values corresponds to the order of the models in the list.
 p_adj_bglm_bt
 
 
-
+# ---------------------------------------------------------------------
 
 
